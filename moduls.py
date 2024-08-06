60052ad15ef08df8d5e0f731207f72101ce9f1cd
Setgey Ivanov <iv4nov00090@yandex.com>
2024-08-06 13:39:00 +0300

    add new file

--- moduls.py
+++ moduls.py
@@ -1,1 +1,9 @@
-print(  )
+from collections import Counter
+
+
+phones = ['iPhone Xs', 'Samsung Galaxy S10', 'Xiaomi M18', 'iPhone Xs', 'iPhone Xs', 'iPhone Xs', 'Samsung Galaxy S10', 'Samsung Galaxy S10', 'Samsung Galaxy S10', 'Xiaomi M18']
+count = Counter(phones)
+
+print(count)
+
+print(count['iPhone Xs'])
